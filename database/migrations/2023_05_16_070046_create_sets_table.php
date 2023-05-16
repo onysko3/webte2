6cbbe3f470c9a8e06210c58f41a5ee5b45bd70f6
OlehOnysko <onysko@mindit.sk>
2023-05-16 09:37:22 +0200

    feat: add database migrations

--- database/migrations/2023_05_16_070046_create_sets_table.php
+++ database/migrations/2023_05_16_070046_create_sets_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('sets', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->boolean('is_teacher')->default(0)->change();
-            $table->rememberToken();
+            $table->string('file_path');
+            $table->string('images_path')->nullable();
+            $table->integer('points')->change()->nullable();
+            $table->date('available_on')->nullable()->change();
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('sets');
     }
 };
