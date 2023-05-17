013d49b3117c91c52ec078a0385fc7a0c7dc4166
MiroslavMalisek <xmalisek@stuba.sk>
2023-05-17 10:25:35 +0000

    merged login with migrations

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -4,23 +4,24 @@ namespace App\Http\Controllers;
 use App\Models\Task;
 use App\Models\User;
 
-use App\Models\TaskModel;
 use Illuminate\Http\Request;
 
 class StudentController extends Controller
 {
 
-    public function getTaskByStudent(Request $request){
+    public function getTaskByStudent(Request $request)
+    {
         //TODO z requestu vytiahnuť ktora uloha sa zobrazi
 
-        $user  = User::find(1);
+        $user = User::find(1);
         $tasks = $user->tasks;
-        return view('student/editor', ['user'=>$user, 'tasks'=>$tasks]);
-      
+        return view('student/editor', ['user' => $user, 'tasks' => $tasks]);
+    }
+
     public function getTask(Request $request){
         //TODO z requestu vytiahnuť ktora uloha sa zobrazi
 
-        $task  = TaskModel::find(4);
+        $task  = Task::find(1);
         return view('student/editor', ['task'=>$task]);
     }
 }
